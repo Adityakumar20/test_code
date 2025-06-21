cbedac5db4bbda4af4d88b590e2459967f8d5b91
ADITYA KUMAR <128803169+Adityakumar20@users.noreply.github.com>
2025-06-21 15:51:53 +0530

    Add OTA update and WiFi to ESP32 test sketch
    Introduces WiFi connectivity and HTTP-based OTA firmware update to the test.ino sketch for ESP32. Also refactors the LED blink logic to use non-blocking timing.

--- test/test.ino
+++ test/test.ino
@@ -1,9 +1,74 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
+
+const char* ssid = "test";  // 🔁 Change this
+const char* password = "12345678";  // 🔁 Change this
+
+const char* firmwareUrl = "https://raw.githubusercontent.com/Adityakumar20/test_code/main/test/build/esp32.esp32.esp32/test.ino.bin";
+
+unsigned long lastBlink = 0;
+bool ledState = false;
+
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(2, OUTPUT);  // On-board LED
+  Serial.begin(115200);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected!");
+
+  // Start OTA update
+  HTTPClient http;
+  http.begin(firmwareUrl);
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    int contentLength = http.getSize();
+    bool canBegin = Update.begin(contentLength);
+
+    if (canBegin) {
+      WiFiClient* client = http.getStreamPtr();
+      size_t written = Update.writeStream(*client);
+      if (written == contentLength) {
+        Serial.println("OTA written successfully.");
+      } else {
+        Serial.println("OTA write failed.");
+      }
+
+      if (Update.end()) {
+        Serial.println("OTA update complete.");
+        if (Update.isFinished()) {
+          Serial.println("Restarting ESP32...");
+          ESP.restart();
+        } else {
+          Serial.println("OTA not finished.");
+        }
+      } else {
+        Serial.println("OTA Error: " + String(Update.getError()));
+      }
+
+    } else {
+      Serial.println("Not enough space for OTA");
+    }
+
+  } else {
+    Serial.println("Firmware download failed. HTTP code: " + String(httpCode));
+  }
+
+  http.end();
 }
+
 void loop() {
-  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  // Blink LED
+  if (millis() - lastBlink > 500) {
+    ledState = !ledState;
+    digitalWrite(2, ledState);
+    lastBlink = millis();
+  }
 }
